0b47bbde931393cf9adc052cdd7526a130e648c3
unknown <Boonie>
2024-08-30 08:29:56 +0700

    Baru

---
+++ file.bat
@@ -0,0 +1,7 @@
+echo "# Registrasi_Mobile" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/NayeonHusband/Registrasi_Mobile.git
+git push -u origin main
